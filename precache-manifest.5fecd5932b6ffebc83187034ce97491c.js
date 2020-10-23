fc6bde8ea2d6b5f175628ba200517db266eb5e9c
Reena Panwar <reenapanwar.23@gmail.com>
2020-10-23 15:13:58 +0530

    Updates

---
+++ precache-manifest.5fecd5932b6ffebc83187034ce97491c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f43b353b7cbe650d1ee7ee172b578f9",
+    "url": "/blueSea/index.html"
+  },
+  {
+    "revision": "6d9fec722819dd9c12b7",
+    "url": "/blueSea/static/css/main.7de292cf.chunk.css"
+  },
+  {
+    "revision": "24f66c4199841780a9c1",
+    "url": "/blueSea/static/js/2.d3ed4e04.chunk.js"
+  },
+  {
+    "revision": "2881c4a89c3c7ad239c40a9c9582b16d",
+    "url": "/blueSea/static/js/2.d3ed4e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9fec722819dd9c12b7",
+    "url": "/blueSea/static/js/main.d79c6a74.chunk.js"
+  },
+  {
+    "revision": "11db83de60d7811efb83",
+    "url": "/blueSea/static/js/runtime-main.fa558caa.js"
+  },
+  {
+    "revision": "8e53927a68bac7b6a3051ab55ac1304c",
+    "url": "/blueSea/static/media/A1.8e53927a.jpg"
+  },
+  {
+    "revision": "9e0e28b6cfb4cb458b2484f2c2c97cc8",
+    "url": "/blueSea/static/media/A1.9e0e28b6.PNG"
+  },
+  {
+    "revision": "253cc2f3d36d23280b403cb10127ee50",
+    "url": "/blueSea/static/media/A2.253cc2f3.jpg"
+  },
+  {
+    "revision": "998c03fdf759e28a4a8ced16e7165100",
+    "url": "/blueSea/static/media/A2.998c03fd.PNG"
+  },
+  {
+    "revision": "d394723f3e1caaf7fd86921638ca1426",
+    "url": "/blueSea/static/media/A2.d394723f.jpg"
+  },
+  {
+    "revision": "1fd92c625b5adaebe260d8f1d63e5982",
+    "url": "/blueSea/static/media/A3.1fd92c62.jpg"
+  },
+  {
+    "revision": "92dee1563227a8fa5813d47e92730115",
+    "url": "/blueSea/static/media/A3.92dee156.PNG"
+  },
+  {
+    "revision": "696c948836c45bf26b74411bace9d404",
+    "url": "/blueSea/static/media/A4.696c9488.jpg"
+  },
+  {
+    "revision": "fb1f8983550a2cbbc0bbde5d843d0951",
+    "url": "/blueSea/static/media/Bg1.fb1f8983.jpg"
+  },
+  {
+    "revision": "ec41405e3fba779857d5f84d6d1c11eb",
+    "url": "/blueSea/static/media/C1.ec41405e.jpg"
+  },
+  {
+    "revision": "2ec05572f237e62947d2b688e74e7cf0",
+    "url": "/blueSea/static/media/D1.2ec05572.jpg"
+  },
+  {
+    "revision": "f45f01f5c8387f9209107e599f4db9da",
+    "url": "/blueSea/static/media/E1.f45f01f5.jpg"
+  },
+  {
+    "revision": "f408a3cc9861b19488c980276ad640c9",
+    "url": "/blueSea/static/media/E2.f408a3cc.jpg"
+  },
+  {
+    "revision": "4cf20585f6f2e5c9008e28693b473470",
+    "url": "/blueSea/static/media/E3.4cf20585.jpg"
+  },
+  {
+    "revision": "4b837e5e1fdffe0208b557eddcc06223",
+    "url": "/blueSea/static/media/M1.4b837e5e.jpg"
+  },
+  {
+    "revision": "984d3367e090ecf1b9c096a8223bcdf8",
+    "url": "/blueSea/static/media/M2.984d3367.jpg"
+  },
+  {
+    "revision": "991bac41ecdd709347df262e14474e40",
+    "url": "/blueSea/static/media/R4.991bac41.jpg"
+  },
+  {
+    "revision": "b6a8a7471de2459e923f7f64f9197d9c",
+    "url": "/blueSea/static/media/S1.b6a8a747.jpg"
+  },
+  {
+    "revision": "d4a795b53a7afc397ed87c252a521f7a",
+    "url": "/blueSea/static/media/S2.d4a795b5.jpg"
+  },
+  {
+    "revision": "3d336987c0c200ef4a1f5a6235a51c59",
+    "url": "/blueSea/static/media/S3.3d336987.jpg"
+  },
+  {
+    "revision": "518587435d13271864eeea591f1a05e9",
+    "url": "/blueSea/static/media/S4.51858743.jpg"
+  },
+  {
+    "revision": "3bfa933fe556ae5431dcee7c7db6333d",
+    "url": "/blueSea/static/media/Sea.3bfa933f.jpg"
+  },
+  {
+    "revision": "413b88649566bd399ebd67210fb92919",
+    "url": "/blueSea/static/media/car.413b8864.PNG"
+  },
+  {
+    "revision": "e7091a1b30044202aa0ab977747ce16b",
+    "url": "/blueSea/static/media/dining.e7091a1b.PNG"
+  },
+  {
+    "revision": "561340bb38fab9f11c68a20fccdde4bf",
+    "url": "/blueSea/static/media/discover.561340bb.PNG"
+  },
+  {
+    "revision": "cd4f511ffe7f9b2fec35edcd1e37e413",
+    "url": "/blueSea/static/media/facilities.cd4f511f.PNG"
+  },
+  {
+    "revision": "d1aaf4264c88f807657748db81ee4f67",
+    "url": "/blueSea/static/media/wedding.d1aaf426.PNG"
+  },
+  {
+    "revision": "6385f6048625572ca1c638337c9830b6",
+    "url": "/blueSea/static/media/weddings.6385f604.PNG"
+  },
+  {
+    "revision": "e16cf44ac2846922f88b06e0b0362932",
+    "url": "/blueSea/static/media/wellness.e16cf44a.PNG"
+  }
+]);
